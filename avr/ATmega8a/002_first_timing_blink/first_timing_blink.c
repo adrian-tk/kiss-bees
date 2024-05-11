cb4061aeb0efea31d777a04cf2e54474bec85233
domel <adrian.tk@gmail.com>
2024-05-11 20:42:07 +0200

    modified comments

--- avr/ATmega8a/002_first_timing_blink/first_timing_blink.c
+++ avr/ATmega8a/002_first_timing_blink/first_timing_blink.c
@@ -1,3 +1,4 @@
+//default frequency for atmega8a
 #define F_CPU 1000000UL
 
 #include <avr/io.h>
@@ -6,11 +7,11 @@ int main(){
    	//configure pin as output
 	DDRB |= (1<<PB0);
 
-	//Set timer with frequency as main clock
-	//TCCR1B |= (1<<CS10);
-	//Set timer with frequency divided by 64
+	//Set timer with frequency | divided by 64
 	TCCR1B |= ((1<<CS10) | (1<<CS11));
-					 
+
+	//main freqency (1 000 000/s) / div (64) = 15625/s
+	//so we need to count to 15624 (zero included) to get 1 second
 
 	for (;;){
 		//checking value of our timer
